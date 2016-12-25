0aa638fd51eedd30791cfdbba8e1f7764a1b8698
Ege <alikemal@riseup.net>
2016-12-25 21:26:42 +0300

    Added new pages to package script

--- package.sh
+++ package.sh
@@ -6,5 +6,8 @@ cp background.js sansursavan-package/background.js
 cp LICENSE sansursavan-package/LICENSE
 cp manifest.json sansursavan-package/manifest.json
 cp welcome.html sansursavan-package/welcome.html
+cp redirect.html sansursavan-package/redirect.html
+cp proxy.html sansursavan-package/proxy.html
+cp redirect.js sansursavan-package/redirect.js
 echo $(web-ext build -s ./sansursavan-package)
 rm -rf sansursavan-package
