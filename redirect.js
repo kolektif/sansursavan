e81c296dbb1af09043a688a7b4bdba2854787d0d
guvenatbakan <guven@atbakan.com>
2016-12-27 00:59:27 +0300

    remove unccessary files

--- redirect.js
+++
@@ -1,6 +0,0 @@
-var urlParams = new URLSearchParams(window.location.search);
-var newUrl = urlParams.get('newUrl');
-var oldUrl = urlParams.get('oldUrl');
-document.querySelector('#oldUrl').innerText = oldUrl;
-document.querySelector('#newUrl').href = newUrl;
-document.querySelector('#newUrl').innerText = newUrl;
