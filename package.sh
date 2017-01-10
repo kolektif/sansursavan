5fbac7f8957c981c4164078d85ec7c83595706de
shibby <guvenatbakan@gmail.com>
2017-01-11 01:39:18 +0300

    popup eklendi.

--- package.sh
+++ package.sh
@@ -2,12 +2,13 @@
 
 mkdir sansursavan-package
 cp -R icons sansursavan-package/icons
-cp background.js sansursavan-package/background.js
+cp js/background.js sansursavan-package/js/background.js
 cp LICENSE sansursavan-package/LICENSE
 cp manifest.json sansursavan-package/manifest.json
+cp popup.html sansursavan-package/popup.html
 cp welcome.html sansursavan-package/welcome.html
 cp redirect.html sansursavan-package/redirect.html
 cp proxy.html sansursavan-package/proxy.html
-cp redirect.js sansursavan-package/redirect.js
+cp js/redirect.js sansursavan-package/js/redirect.js
 echo $(web-ext build -s ./sansursavan-package)
 rm -rf sansursavan-package
