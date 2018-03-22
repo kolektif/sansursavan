f7b58a4f28477a34c172193e4b1c3ee62ff04da5
Güven Atbakan <shibby@users.noreply.github.com>
2018-03-22 22:29:03 +0300

    Update redirect.js
    oldUrl i linkliyoruz

--- redirect.js
+++ redirect.js
@@ -1,6 +1,7 @@
 var urlParams = new URLSearchParams(window.location.search);
 var newUrl = urlParams.get('newUrl');
 var oldUrl = urlParams.get('oldUrl');
+document.querySelector('#oldUrl').href = oldUrl;
 document.querySelector('#oldUrl').innerText = oldUrl;
 document.querySelector('#newUrl').href = newUrl;
 document.querySelector('#newUrl').innerText = newUrl;
